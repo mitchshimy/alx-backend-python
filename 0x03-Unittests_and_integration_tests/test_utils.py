519f26fc73bbe140d3274b121ae62b4610969fee
ShimY <42277453+mitchshimy@users.noreply.github.com>
2025-05-29 11:07:20 +0300

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -100,7 +100,6 @@ class TestMemoize(unittest.TestCase):
         test_instance = TestClass()
 
         with patch.object(test_instance, 'a_method', return_value=42) as mock_method:
-            # Call twice, second call should use cached value
             result1 = test_instance.a_property
             result2 = test_instance.a_property
 
