682d4aed9a672c946e2b44804d3c44110008be4e
Mitchshimy <mitchshimy@gmail.com>
2025-06-13 10:31:26 +0300

    Django chat

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,6 +1,8 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Message, Notification
+from .models import Message, Notification, MessageHistory
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_delete
 
 @receiver(post_save, sender=Message)
 def create_notification_on_message(sender, instance, created, **kwargs):
@@ -9,3 +11,12 @@ def create_notification_on_message(sender, instance, created, **kwargs):
             user=instance.receiver,
             message=instance
         )
+User = get_user_model()
+@receiver(post_delete, sender=User)
+def delete_user_related_data(sender, instance, **kwargs):
+    print(f"[Signal] Deleting data for user: {instance}")
+    # If not using CASCADE, manually delete:
+    Message.objects.filter(sender=instance).delete()
+    Message.objects.filter(receiver=instance).delete()
+    Notification.objects.filter(user=instance).delete()
+    MessageHistory.objects.filter(message__sender=instance).delete()
