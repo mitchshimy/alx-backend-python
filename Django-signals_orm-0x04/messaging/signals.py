7a471e012ee92f39ec11eba4fac5fe48f423efd5
Mitchshimy <mitchshimy@gmail.com>
2025-06-13 11:19:39 +0300

    Set up basic caching

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -3,6 +3,7 @@ from django.dispatch import receiver
 from .models import Message, Notification, MessageHistory
 from django.contrib.auth import get_user_model
 from django.db.models.signals import post_delete
+from django.db.models.signals import pre_save
 
 @receiver(post_save, sender=Message)
 def create_notification_on_message(sender, instance, created, **kwargs):
@@ -20,3 +21,17 @@ def delete_user_related_data(sender, instance, **kwargs):
     Message.objects.filter(receiver=instance).delete()
     Notification.objects.filter(user=instance).delete()
     MessageHistory.objects.filter(message__sender=instance).delete()
+
+@receiver(pre_save, sender=Message)
+def log_message_edit(sender, instance, **kwargs):
+    if instance.pk:
+        try:
+            old_message = Message.objects.get(pk=instance.pk)
+            if old_message.content != instance.content:
+                MessageHistory.objects.create(
+                    message=old_message,
+                    old_content=old_message.content
+                )
+                instance.edited = True
+        except Message.DoesNotExist:
+            pass
